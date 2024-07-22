bd03b25b634ff13387f36cba3b2c2a42d014e8ea
Isabel <isabel.salo@hotmail.com>
2024-07-22 12:11:49 +0200

    Added more tests for Player

--- tests/Card5/PlayerTest.php
+++ tests/Card5/PlayerTest.php
@@ -102,12 +102,86 @@ class PlayerTest extends TestCase
         $this->assertTrue($player->hasFolded());
     }
 
-    public function testDecideCardsToSwap()
+    public function testDecideCardsToSwapStraightFlush()
     {
         $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Straight Flush');
 
-        // Mocking the HandEvaluator for testing purposes
-        $handEvaluator->method('evaluateHand')->willReturn('High Card');
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('hearts', '3'),
+            new CardGraphics('hearts', '4'),
+            new CardGraphics('hearts', '5'),
+            new CardGraphics('hearts', '6')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEmpty($swapIndices);
+    }
+
+    public function testDecideCardsToSwapFourOfAKind()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Four of a Kind');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('diamonds', '2'),
+            new CardGraphics('clubs', '2'),
+            new CardGraphics('spades', '2'),
+            new CardGraphics('hearts', '6')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEmpty($swapIndices);
+    }
+
+    public function testDecideCardsToSwapFullHouse()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Full House');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('diamonds', '2'),
+            new CardGraphics('clubs', '2'),
+            new CardGraphics('spades', '3'),
+            new CardGraphics('hearts', '3')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEmpty($swapIndices);
+    }
+
+    public function testDecideCardsToSwapFlush()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Flush');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('hearts', '4'),
+            new CardGraphics('hearts', '6'),
+            new CardGraphics('hearts', '8'),
+            new CardGraphics('hearts', '10')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEmpty($swapIndices);
+    }
+
+    public function testDecideCardsToSwapStraight()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Straight');
 
         $player = new Player('Player 1', $handEvaluator);
 
@@ -120,6 +194,101 @@ class PlayerTest extends TestCase
         ]);
 
         $swapIndices = $player->decideCardsToSwap();
-        $this->assertNotEmpty($swapIndices);
+        $this->assertEmpty($swapIndices);
+    }
+
+    public function testDecideCardsToSwapThreeOfAKind()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Three of a Kind');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('diamonds', '2'),
+            new CardGraphics('clubs', '2'),
+            new CardGraphics('spades', '4'),
+            new CardGraphics('hearts', '6')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEquals([3, 4], $swapIndices);
+    }
+
+    public function testDecideCardsToSwapTwoPair()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Two Pair');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('diamonds', '2'),
+            new CardGraphics('clubs', '4'),
+            new CardGraphics('spades', '4'),
+            new CardGraphics('hearts', '6')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEquals([4], $swapIndices);
+    }
+
+    public function testDecideCardsToSwapOnePair()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('One Pair');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('diamonds', '2'),
+            new CardGraphics('clubs', '4'),
+            new CardGraphics('spades', '5'),
+            new CardGraphics('hearts', '6')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEquals([2, 3, 4], $swapIndices);
+    }
+
+    public function testDecideCardsToSwapHighCard()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('High Card');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('diamonds', '4'),
+            new CardGraphics('clubs', '6'),
+            new CardGraphics('spades', '8'),
+            new CardGraphics('hearts', '10')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEquals([0, 1, 2], $swapIndices);
+    }
+
+    public function testDecideCardsToSwapUnknownHand()
+    {
+        $handEvaluator = $this->createMock(HandEvaluator::class);
+        $handEvaluator->method('evaluateHand')->willReturn('Unknown');
+
+        $player = new Player('Player 1', $handEvaluator);
+
+        $player->receiveCards([
+            new CardGraphics('hearts', '2'),
+            new CardGraphics('diamonds', '4'),
+            new CardGraphics('clubs', '6'),
+            new CardGraphics('spades', '8'),
+            new CardGraphics('hearts', '10')
+        ]);
+
+        $swapIndices = $player->decideCardsToSwap();
+        $this->assertEquals([0, 1, 2], $swapIndices);
     }
 }
